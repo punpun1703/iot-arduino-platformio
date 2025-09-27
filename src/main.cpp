b632b7c98689269832d2421fdacf14c1d8fdd4c4
punpun1703 <punpun741852963@gmail.com>
2025-09-27 21:33:28 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED_PIN 2   // LED บนบอร์ด ESP32 Dev ใช้ขา GPIO2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);  // เปิด LED
+  delay(500);
+  digitalWrite(LED_PIN, LOW);   // ปิด LED
+  delay(500);
+}
